85e620bdebc1db5cf08ca70c89a2e44fe562b19f
Dorofeyev Dmytro <dorofeyev74@gmail.com>
2026-08-31 15:02:38 +0200

    Fix note details client location

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,9 +1,5 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query';
-import NoteDetailsClient from '@/app/notes/NoteDetails.client';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import NoteDetailsClient from '@/app/notes/[id]/NoteDetails.client';
 import { fetchNoteById } from '@/lib/api';
 
 type Props = {
